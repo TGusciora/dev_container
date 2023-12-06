77e3b242bed2526eca229a5ecbfaaf27f294ab59
TGusciora <tgusciora@gmail.com>
2023-12-06 16:58:20 +0100

    2023/12/06
    - docker updates

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+import redis
 
+r = redis.Redis(host='localhost', port=6379)
+)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World2 mordeczko Szoko bons twój wons"}
+
+@app.get("/hits")
+def read_root():
+    r.incr('hits')
+    return {"hits": r.get('hits')}
